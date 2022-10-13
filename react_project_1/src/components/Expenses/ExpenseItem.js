af5866750e77af26527d66606007201093e22b49
anirudh ramaswamy <laidbackanonymous@gmail.com>
2022-10-13 15:29:59 +0530

    example code implementation without use of states

--- react_project_1/src/components/Expenses/ExpenseItem.js
+++ react_project_1/src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,22 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
+  //Event Handlers functions
+  const clickHandler = () => {
+    console.log("clicked !!");
+    title = "updated";
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">{props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
