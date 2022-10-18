138cd5d10a720a513a04fb1b7681cd60bd45f982
anirudh ramaswamy <laidbackanonymous@gmail.com>
2022-10-18 15:52:20 +0530

    bottom up data codes added

--- react_project_1/src/components/NewExpense/ExpenseForm.js
+++ react_project_1/src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   //states
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
@@ -47,7 +47,7 @@ const ExpenseForm = () => {
       anount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
