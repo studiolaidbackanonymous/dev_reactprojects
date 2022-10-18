610fd558325de87c454b9b2d282818b442f5f8ca
anirudh ramaswamy <laidbackanonymous@gmail.com>
2022-10-18 12:03:24 +0530

    added multiple states in ExpenseForm components

--- react_project_1/src/components/NewExpense/ExpenseForm.js
+++ react_project_1/src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  //states
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  //handlre functions
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    console.log(enteredTitle);
+  };
+
+  const amountChnageHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    console.log(enteredAmount);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    console.log(enteredDate);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
-          <label>Titles</label>
-          <input type="text"></input>
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler}></input>
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01"></input>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChnageHandler}
+          ></input>
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-01-01" step="2022-12-31"></input>
+          <input
+            type="date"
+            min="2022-01-01"
+            step="2022-12-31"
+            onChange={dateChangeHandler}
+          ></input>
         </div>
       </div>
       <div className="new-expense__actions">
