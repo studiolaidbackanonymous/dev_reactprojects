85e7c3e6bd9fce418e05944521c9f2b4d49c24c1
anirudh ramaswamy <laidbackanonymous@gmail.com>
2022-10-19 15:54:50 +0530

    rendering list with keys

--- react_project_1/src/App.js
+++ react_project_1/src/App.js
@@ -1,33 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const dummyExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  // “When principles that run against your deepest convictions begin to win the day, then battle is your calling, and peace has become sin; you must, at the price of dearest peace, lay your convictions bare before friend and enemy, with all the fire of your faith.”
-  // ― Abraham Kuyper
-
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
+  const [expenses, setExpenses] = useState(dummyExpenses);
   //old style
 
   // return React.createElement(
@@ -39,9 +37,10 @@ const App = () => {
 
   //handler function
 
-  const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+  const addExpenseHandler = (newExpenses) => {
+    setExpenses((prevExpenses) => {
+      return [newExpenses, ...prevExpenses];
+    });
   };
 
   return (
--- react_project_1/src/components/Expenses/ExpenseItem.js
+++ react_project_1/src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log("expense item excuted");
-
-  //Event Handlers functions
-  const clickHandler = () => {
-    setTitle("updated");
-    console.log("click handler");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">{props.amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
