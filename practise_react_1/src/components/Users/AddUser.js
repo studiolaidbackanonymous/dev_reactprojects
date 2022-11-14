f9dcd575870c868bbaf38e280ee1d7e872f155ad
anirudh ramaswamy <laidbackanonymous@gmail.com>
2022-11-14 14:54:08 +0530

    added UserList module

--- practise_react_1/src/components/Users/AddUser.js
+++ practise_react_1/src/components/Users/AddUser.js
@@ -9,6 +9,13 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) return;
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   const usernameChangeHandler = (event) => {
@@ -25,11 +32,17 @@ const AddUser = (props) => {
         <label htmlFor="username">Username</label>
         <input
           id="username"
-          type="teaxt"
+          type="text"
+          value={enteredUsername}
           onChange={usernameChangeHandler}
         ></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler}></input>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        ></input>
         <Button type="submit"> Add User</Button>
       </form>
     </Card>
