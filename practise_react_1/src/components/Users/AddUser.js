3af304f120adeba00fe54b89b36c6a94ab7093f1
anirudh ramaswamy <laidbackanonymous@gmail.com>
2022-11-21 12:13:29 +0530

    wrrapper added

--- practise_react_1/src/components/Users/AddUser.js
+++ practise_react_1/src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
@@ -45,7 +46,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -72,7 +73,7 @@ const AddUser = (props) => {
           <Button type="submit"> Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
